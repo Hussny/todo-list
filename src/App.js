6fd5de9305df62ae86efe6dff93d689ef60ba788
Hussny <husseinsalat24@gmail.com>
2020-09-02 22:12:04 +0300

    Add buttons in App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ function App() {
   return (
     <>                     
     <TodoList /> 
-    <input type="text" />    
+    <input type="text" />
+    <button>Add Todo</button>    
+    <button>Clear Completed</button>
+    <div>0 left to do</div>
     </>                     
   )
 }
